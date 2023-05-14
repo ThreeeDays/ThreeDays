ca668838dfdfe2f416f4813d4b2189c86395b69a
sudenaz <108553141+SudenazKTC@users.noreply.github.com>
2023-05-15 00:11:02 +0300

    header menu ok

---
+++ script.js
@@ -0,0 +1,12 @@
+/* sonra */
+var btn=document.querySelector("#btn");
+var sidebar=document.querySelector(".sidebar");
+var search=document.querySelector(".fa-sharp");
+
+function button(){
+    sidebar.classList.toggle("active");
+}
+
+function src(){
+    sidebar.classList.toggle("active");
+}
